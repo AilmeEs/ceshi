62dc84d208ebc82c7e1f63a9902f9a90042faefc
wensong <1009489901@qq.com>
2019-03-20 20:43:14 +0800

    补丁文件

--- article/views.py
+++ article/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render,get_object_or_404
 from .models import Post
+from .forms import PostForm
 # Create your views here.
 
 
@@ -11,3 +12,12 @@ def delete_(request,id):
 def index(request):
     posts=Post.objects.all()
     return render(request,'index.html',{'posts':posts})
+def edit(request,id):
+    po=get_object_or_404(Post,id=id)
+    if request.method=="POST":
+        po_forms=PostForm(data=request.POST,instance=po)
+        if po_forms.is_valid():
+            po_forms.save()
+        return redirect("index")
+    else:
+        po_forms=PostForm(instance=po)
